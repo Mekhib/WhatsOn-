dc6f72d014c82e4a688d74501443a41e866ca9ef
Mekhib <Mekhi19132@gmail.com>
2020-02-20 10:59:57 -0800

    created grid layout; added another API for info; added icons and css effects; 60% complete

--- public/js/resultsPage.js
+++ public/js/resultsPage.js
@@ -5,9 +5,44 @@ $(document).ready(function() {
     type: "GET",
     url: `/query/${data}`
   }).then(res => {
+    var id = res[0].show.externals.imdb;
+    console.log(id);
     console.log(res);
-    $(".jumbotron").css({
+    $(".jumbotron-background").css({
       "background-image": `url(${res[0].show.image.medium})`
     });
+    $("#title").text(res[0].show.name);
+    $("#info").html(res[0].show.summary);
+    $("#genre").html(res[0].show.genres[0]);
+    var stars = Math.floor(res[0].show.rating.average);
+    console.log(stars);
+    for (var i = 0; i < stars; i++) {
+      console.log("loop");
+      var html =
+        "<img src='https://img.icons8.com/emoji/48/000000/star-emoji.png'></img>";
+      $("#align").append(html);
+    }
+    $("#count").html(`${stars} out of 10`);
+    getID();
   });
+  getID = () => {
+    var string = data.replace("+", "%20");
+    console.log(string);
+    var apiKey = "0df7e9832c7f3dbefb03f430a97b5c7a";
+    $.ajax({
+      type: "GET",
+      url: `https://api.themoviedb.org/3/search/tv?query=${string}&api_key=${apiKey}&language=en-US&page=1`
+    }).then(res => {
+      showData(res.results[0].id);
+    });
+  };
+  showData = id => {
+    var apiKey = "0df7e9832c7f3dbefb03f430a97b5c7a";
+    $.ajax({
+      type: "GET",
+      url: `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`
+    }).then(res => {
+      console.log(res);
+    });
+  };
 });
