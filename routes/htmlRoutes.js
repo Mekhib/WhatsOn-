6d5c950e2c45f183b71622e52201f4c27e85aa7d
Mekhib <Mekhi19132@gmail.com>
2020-02-21 14:13:24 -0800

    Deleted unneeded code.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
   app.get("/queryresults", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/html/resultsPage.html"));
   });
+  //NO RESULTS FOUND PAGE NEEDED!
   // app.get("*", function(req, res) {
   //   res.render("404");
   // });
--- server.js
+++ server.js
@@ -9,19 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
 
 app.listen(PORT, function() {
   console.log(
